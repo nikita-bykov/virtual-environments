ae2bd276eda010aa204da29711bd355ac05a2634
Nikita Bykov <v-nibyko@microsoft.com>
2021-01-27 12:54:12 +0300

    updated ghcup installation

--- images/linux/scripts/installers/haskell.sh
+++ images/linux/scripts/installers/haskell.sh
@@ -7,14 +7,15 @@
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/etc-environment.sh
 
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-export PATH="$HOME/.ghcup/bin:$PATH"
-echo 'export PATH="$PATH:$HOME/.ghcup/bin"' >> "$HOME/.bashrc"
+mkdir -p /usr/bin/.ghcup/bin
+curl https://gitlab.haskell.org/haskell/ghcup/raw/master/ghcup -o /usr/bin/.ghcup/bin/ghcup
+chmod +x /usr/bin/.ghcup/bin/ghcup
+export PATH="/usr/bin/.cabal/bin:/usr/bin/.ghcup/bin:$PATH"
 
-# ghcup output looks like this "ghc 8.6.4 base-4.12.0.0 hls-powered", need to take all the first versions only(8.6.4 in that case) and avoid pre-release ones
+ghcup upgrade
+
 availableVersions=$(ghcup list -t ghc -r | grep -v "prerelease" | awk '{print $2}')
 
-# Install 3 latest major versions(For instance 8.6.5, 8.8.4, 8.10.2)
 minorMajorVersions=$(echo "$availableVersions" | cut -d"." -f 1,2 | uniq | tail -n3)
 for majorMinorVersion in $minorMajorVersions; do
     fullVersion=$(echo "$availableVersions" | grep "$majorMinorVersion." | tail -n1)
