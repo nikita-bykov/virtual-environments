5af1ed3afd3542047bb767290d0a0436b2f6727c
Nikita Bykov <v-nibyko@microsoft.com>
2021-07-05 13:37:58 +0300

    Move choco installation list into toolset

--- images/win/scripts/Installers/Install-CommonUtils.ps1
+++ images/win/scripts/Installers/Install-CommonUtils.ps1
@@ -2,14 +2,6 @@ $commonPackages = (Get-ToolsetContent).choco.common_packages
 
 foreach ($package in $commonPackages)
 {
-    Write-Host "Installing $package"
-    Choco-Install -PackageName $package
-}
-
-$commonPackages = (Get-ToolsetContent).choco.common_packages_with_args
-
-foreach ($package in $commonPackages)
-{
     Write-Host "Installing ${package.name}"
     Choco-Install -PackageName $package.name -ArgumentList $package.args
 }
